d386987681f1230f72bf19934215c1f2b056e214
Hegehog <workjob2077@gmail.com>
2024-12-02 18:19:22 +0300

    Исправление 2

--- Home.cpp
+++ Home.cpp
@@ -22,3 +22,22 @@ void Home::setNumFloor(int numFloor_p) {
 Apartment* Home::getApartmentArray() const {
     return apartmentArr;
 }
+
+Home::Home(int numApart_p, int numFloor_p) {
+    if (numApart_p < 1 || numFloor_p < 1)
+        exit(368); // Ошибка: неверные размеры данных
+
+    numApart = numApart_p;
+    apartmentArr = new Apartment[numApart];
+    numFloor = numFloor_p;
+}
+
+Home::Home(const Home& obj) {
+    if (this != &obj) {
+        delete[] apartmentArr;
+
+        numApart = obj.numApart;
+        numFloor = obj.numFloor;
+        apartmentArr = obj.apartmentArr;
+    }
+}
